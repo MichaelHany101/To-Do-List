6a8bb0b0f1f2adbe76e721bdb7b905377d55bf33
Michael <michaelhany101@gmail.com>
2023-05-24 16:13:51 +0300

    TabBar

---
+++ To-Do List/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  To-Do List
+//
+//  Created by Michael Hany on 24/05/2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.changeTabBarRadius()
+        // Do any additional setup after loading the view.
+    }
+    
+    func changeTabBarRadius() {
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.isTranslucent = true
+        self.tabBar.layer.cornerRadius = 50
+        self.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
